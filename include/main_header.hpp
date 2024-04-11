de5ba8a675e25883fdc37d92531828da01d98794
home_server <lorris.belus@icloud.com>
2024-04-11 13:11:27 +0000

    [lbs] add CLI and basic function pointer cmds

---
+++ include/main_header.hpp
@@ -0,0 +1,11 @@
+#ifndef __MAIN_HEADERFILE_
+#define __MAIN_HEADERFILE_
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <my_getopt.hpp>
+#include <main_CLI_header.h>
+#include <my_cmd.hpp>
+
+#endif
