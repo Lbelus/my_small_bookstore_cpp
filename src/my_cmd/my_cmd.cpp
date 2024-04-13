a2f6cc7698d08b9f8f05b0f7bcce0ddbebe9442e
home_server <lorris.belus@icloud.com>
2024-04-13 14:20:10 +0000

    [lbs] light refactor

--- src/my_cmd/my_cmd.cpp
+++ src/my_cmd/my_cmd.cpp
@@ -9,19 +9,26 @@ cmd_ptr_t cmd_ptr_map[] = {
     {NULL, nullptr}
 };
 
+#define _BD_ARG_ "bd"
+#define _LIVRE_ARG_ "livre"
+#define _AUTEUR_ARG_ "auteur"
+#define _BOOKS_ARG_ "books"
+
 int addEntry(MyGetOpt& GetOptObj,  Library& library)
 {
-    if (_my_strcmp(GetOptObj.getArgs()[1], "bd") == 0)
+    char** args = GetOptObj.getArgs();
+    const std::string cmd = args[1];
+    if (cmd == _BD_ARG_)
     {
-        createBd(library, GetOptObj.getArgs()[2], GetOptObj.getArgs()[3], GetOptObj.getArgs()[4]);
+        createBd(library, args[2], args[3], args[4]);
     }
-    else if (_my_strcmp(GetOptObj.getArgs()[1], "livre") == 0)
+    else if (cmd == _LIVRE_ARG_)
     {
-        createLivre(library, GetOptObj.getArgs()[2], GetOptObj.getArgs()[3], GetOptObj.getArgs()[4]);
+        createLivre(library, args[2], args[3], args[4]);
     }
-    else if (_my_strcmp(GetOptObj.getArgs()[1], "auteur") == 0)
+    else if (cmd == _AUTEUR_ARG_)
     {
-        library.addAuthor(GetOptObj.getArgs()[2]);
+        library.addAuthor(args[2]);
     }
     return EXIT_SUCCESS;
 }
@@ -46,44 +53,48 @@ void createLivre(Library& library, const std::string& title, const std::string& 
 
 int listEntry(MyGetOpt& GetOptObj,  Library& library)
 {
-    if (_my_strcmp(GetOptObj.getArgs()[1], "books") == 0)
+    char** args = GetOptObj.getArgs();
+    const std::string cmd = args[1];
+    if (cmd == _BOOKS_ARG_)
     {
         library.displayItems();
     }
-    else if (_my_strcmp(GetOptObj.getArgs()[1], "auteur") == 0)
+    else if (cmd == _AUTEUR_ARG_)
     {
         library.displayAuthors();
     }
     return EXIT_SUCCESS;
 }
 
-
 int findEntry(MyGetOpt& GetOptObj,  Library& library)
 {
-    if (_my_strcmp(GetOptObj.getArgs()[1], "auteur") == 0)
+    char** args = GetOptObj.getArgs();
+    const std::string cmd = args[1];
+    if (cmd == _AUTEUR_ARG_)
     {
-        library.findAuthorByBookTitle(GetOptObj.getArgs()[2]);
+        library.findAuthorByBookTitle(args[2]);
     }
-    else if (_my_strcmp(GetOptObj.getArgs()[1], "books") == 0)
+    else if (cmd == _BOOKS_ARG_)
     {
-        library.findBooksByAuthor(GetOptObj.getArgs()[2]);
+        library.findBooksByAuthor(args[2]);
     }
     return EXIT_SUCCESS;
 }
 
 int removeEntry(MyGetOpt& GetOptObj,  Library& library)
 {
-    if (_my_strcmp(GetOptObj.getArgs()[1], "bd") == 0 || _my_strcmp(GetOptObj.getArgs()[1], "livre") == 0)
+    char** args = GetOptObj.getArgs();
+    const std::string cmd = args[1];
+    if (cmd == _BD_ARG_|| cmd == _LIVRE_ARG_)
     {
-        library.removeItem(GetOptObj.getArgs()[2], GetOptObj.getArgs()[3]);
+        library.removeItem(args[2], args[3]);
     }
-    else if (_my_strcmp(GetOptObj.getArgs()[1], "auteur") == 0)
+    else if (cmd == _AUTEUR_ARG_)
     {
-        library.removeAuthor(GetOptObj.getArgs()[2]);
+        library.removeAuthor(args[2]);
     }
     return EXIT_SUCCESS;
 }
-
 
 int quit(MyGetOpt& GetOptObj,  Library& library)
 {
@@ -95,7 +106,6 @@ int quit(MyGetOpt& GetOptObj,  Library& library)
 int execute_cmd(MyGetOpt& GetOptObj,  Library& library)
 {
     cmd_ptr_t* cf_ptr = cmd_ptr_map;
-
     while (cf_ptr->cmd != NULL)
     {
         if (_my_strcmp(GetOptObj.getKeyword(), cf_ptr->cmd) == 0)
